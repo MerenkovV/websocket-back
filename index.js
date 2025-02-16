0f9e09a0204975a5bc629eb0aa69a5b233b85907
vladislav.merenkov <vladislav.merenkov@rubius.com>
2025-02-16 17:21:25 +0700

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const WebSocket = require("ws");
+const cors = require("cors");
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
+app.use(express.json());
+
 const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 const wss = new WebSocket.Server({ server });
@@ -30,6 +34,10 @@ wss.on("connection", (ws) => {
     clients = clients.filter((client) => client !== ws);
     console.log("Client disconnected");
   });
+});
+
+app.get("/", (request, response) => {
+  response.status(200).json({ message: "HEY!" });
 });
 
 app.use(express.static("public"));
